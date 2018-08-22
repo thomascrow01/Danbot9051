1f0f5b25763f760cf6884659305710a7fa004846
joestaen <35913191+joestaen@users.noreply.github.com>
2018-08-22 16:34:14 +1000

    fixed alone so it doesnt jumpscare me

--- alone.py
+++ alone.py
@@ -7,13 +7,13 @@ def alone():
     font_type = ImageFont.truetype("segoeprb.ttf", 16)
     draw = ImageDraw.Draw(image)
     draw.text(xy=(355,70),text="Alone on a",font=font_type)
-    draw.text(xy=(360,95),text=str(now.strftime('%A')),font=font_type)
+    draw.text(xy=(355,95),text=str(now.strftime('%A')),font=font_type)
     if now.hour >=0 and now.hour < 12:
         draw.text(xy=(370,120),text="morning?",font=font_type)
     if now.hour >=12 and now.hour < 18:
-        draw.text(xy=(370,120),text="afternoon?",font=font_type)
+        draw.text(xy=(355,120),text="afternoon?",font=font_type)
     if now.hour >=18 and now.hour < 24:
         draw.text(xy=(370,120),text="night?",font=font_type)
-    image.show()
+    #image.show()
     image.save("aloneout.png")
 
