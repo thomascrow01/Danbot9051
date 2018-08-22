c2c88e6b0a941d4600c383726dc200d5472a3f92
joestaen <35913191+joestaen@users.noreply.github.com>
2018-08-22 16:39:06 +1000

    fixed text scale

--- alone.py
+++ alone.py
@@ -4,16 +4,16 @@ import calendar
 now = datetime.datetime.now()
 def alone():
     image = Image.open('alone blank.png')
-    font_type = ImageFont.truetype("segoeprb.ttf", 16)
+    font_type = ImageFont.truetype("segoeprb.ttf", 20)
     draw = ImageDraw.Draw(image)
-    draw.text(xy=(355,70),text="Alone on a",font=font_type)
-    draw.text(xy=(355,95),text=str(now.strftime('%A')),font=font_type)
+    draw.text(xy=(340,70),text="Alone on a",font=font_type)
+    draw.text(xy=(340,95),text=str(now.strftime('%A')),font=font_type)
     if now.hour >=0 and now.hour < 12:
-        draw.text(xy=(370,120),text="morning?",font=font_type)
+        draw.text(xy=(355,120),text="morning?",font=font_type)
     if now.hour >=12 and now.hour < 18:
-        draw.text(xy=(355,120),text="afternoon?",font=font_type)
+        draw.text(xy=(340,120),text="afternoon?",font=font_type)
     if now.hour >=18 and now.hour < 24:
-        draw.text(xy=(370,120),text="night?",font=font_type)
+        draw.text(xy=(355,120),text="night?",font=font_type)
     #image.show()
     image.save("aloneout.png")
 
