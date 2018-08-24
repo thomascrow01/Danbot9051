425de1ff4309ebf489a975ef9361e143cfa39fe8
joestaen <35913191+joestaen@users.noreply.github.com>
2018-08-24 16:32:27 +1000

    centered alone function

--- alone.py
+++ alone.py
@@ -3,18 +3,23 @@ import datetime
 import calendar
 
 def alone():
+    W=160
     now = datetime.datetime.now()
     image = Image.open('alone blank.png')
     font_type = ImageFont.truetype("segoeprb.ttf", 20)
     draw = ImageDraw.Draw(image)
+    (dwidth,dheight) = font_type.getsize(str(now.strftime('%A')))
+    (mwidth,mheight) = font_type.getsize("morning?")
+    (awidth,mheight) = font_type.getsize("afternoon?")
+    (nwidth,mheight) = font_type.getsize("night?")
     draw.text(xy=(340,70),text="Alone on a",font=font_type)
-    draw.text(xy=(340,95),text=str(now.strftime('%A')),font=font_type)
+    draw.text(xy=(315+(W-dwidth)/2,95),text=str(now.strftime('%A')),font=font_type)
     if now.hour >=0 and now.hour < 12:
-        draw.text(xy=(345,120),text="morning?",font=font_type)
+        draw.text(xy=((W-mwidth)/2+315,120),text="morning?",font=font_type)
     if now.hour >=12 and now.hour < 18:
-        draw.text(xy=(340,120),text="afternoon?",font=font_type)
+        draw.text(xy=((W-awidth)/2+315,120),text="afternoon?",font=font_type)
     if now.hour >=18 and now.hour < 24:
-        draw.text(xy=(365,120),text="night?",font=font_type)
+        draw.text(xy=((W-nwidth)/2+315,120),text="night?",font=font_type)
     #image.show()
     image.save("aloneout.png")
-
+alone()
